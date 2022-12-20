672fd27e09b729223cfa803e4c55a9536fa7d9b5
Preston Harrison <preston@prestonharrison.com>
2022-12-20 11:27:55 -0500

    setup

--- contracts/mocks/MockToken.sol
+++ contracts/mocks/MockToken.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.16;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
+    // solhint-disable-next-line no-empty-blocks
     constructor() ERC20("Mock Token", "MOCK") {}
 
     function mint(address to, uint256 amount) external {
